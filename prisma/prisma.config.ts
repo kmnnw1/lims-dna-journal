de1ab63ac78085451966e2bdf31da8ca52cb947d
kmnnw1 <kmnnw11044@gmail.com>
2026-04-17 02:09:05 +0700

    fix(prisma): удалено свойство adapter из prisma.config.ts (Prisma 7 compatibility)

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,8 +1,5 @@
-import { defineConfig } from '@prisma/config';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-
-const adapter = new PrismaBetterSqlite3({ url: 'file:./prisma/dev.db' });
-
 export default defineConfig({
-    adapter,
+    datasource: {
+        url: 'file:./dev.db',
+    },
 });
