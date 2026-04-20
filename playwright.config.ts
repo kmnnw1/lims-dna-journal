a88bd086b14371a50ab68c2a04b91e1446da5da3
kmnnw1 <kmnnw11044@gmail.com>
2026-04-20 09:40:07 +0700

    feat: оптимизация конфигурации Playwright для CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,9 +9,9 @@ export default defineConfig({
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	workers: process.env.CI ? 2 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: 'html',
+	reporter: process.env.CI ? [['github'], ['html']] : 'html',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('')`. */
@@ -27,19 +27,18 @@ export default defineConfig({
 			name: 'chromium',
 			use: { ...devices['Desktop Chrome'] },
 		},
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] },
-		},
-		// --- Добавляем мобильные устройства ---
-		{
-			name: 'Mobile Chrome',
-			use: { ...devices['Pixel 5'] },
-		},
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
+		...(!process.env.CI
+			? [
+					{
+						name: 'firefox',
+						use: { ...devices['Desktop Firefox'] },
+					},
+					{
+						name: 'Mobile Chrome',
+						use: { ...devices['Pixel 5'] },
+					},
+				]
+			: []),
 	],
 
 	/* Run your local dev server before starting the tests */
