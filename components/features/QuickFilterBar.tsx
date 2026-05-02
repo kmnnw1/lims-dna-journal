7aff37fde08dc9ce8e51724d9971cfcaa30ef399
kmnnw1 <kmnnw11044@gmail.com>
2026-05-02 23:39:55 +0700

    feat: добавление варианта '5' в селектор количества маркеров

--- components/features/QuickFilterBar.tsx
+++ components/features/QuickFilterBar.tsx
@@ -212,6 +212,7 @@ export function QuickFilterBar({
 								<option value="2">2</option>
 								<option value="3">3</option>
 								<option value="4">4</option>
+								<option value="5">5</option>
 							</select>
 						</div>
 					</div>
